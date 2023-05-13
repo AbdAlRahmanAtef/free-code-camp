4fae448c66144033f61fa05b46c85be14caf8dca
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2023-05-13 06:27:27 +0300

    init

--- src/Components/TopicItem.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@mui/material';
-import React from 'react';
-
-const TopicItem = ({ topic, handleClick }) => {
-  return <Box onClick={handleClick}>{topic[1]}</Box>;
-};
-
-export default TopicItem;
